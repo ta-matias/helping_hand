d427b9b5a1d91d19f336d3dc56e741ac03bb0528
ta-matias <48917278+ta-matias@users.noreply.github.com>
2021-05-23 13:50:02 +0100

    android app added

--- android/HelpingHand/app/src/main/java/pt/unl/fct/di/apdc/helpinghand/utility/ClientConfigs.java
+++ android/HelpingHand/app/src/main/java/pt/unl/fct/di/apdc/helpinghand/utility/ClientConfigs.java
@@ -3,7 +3,5 @@ package pt.unl.fct.di.apdc.helpinghand.utility;
 public class ClientConfigs {
 
     public static final String REST_API_URL = "https://helping-hand-312416.ey.r.appspot.com/";
-            //"https://firstwebapp-307910.nw.r.appspot.com/";
-            //"https://secure-site-307912.appspot.com";
-            //"https://helping-hand-312416.ey.r.appspot.com/";
+            
 }
