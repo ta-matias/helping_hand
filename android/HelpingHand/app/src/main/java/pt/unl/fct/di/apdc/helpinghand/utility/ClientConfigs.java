e94996f44a755cddea804f156256f2f6909c19d0
ta-matias <48917278+ta-matias@users.noreply.github.com>
2021-05-23 17:21:45 +0100

    Edited Link for backEnd

--- android/HelpingHand/app/src/main/java/pt/unl/fct/di/apdc/helpinghand/utility/ClientConfigs.java
+++ android/HelpingHand/app/src/main/java/pt/unl/fct/di/apdc/helpinghand/utility/ClientConfigs.java
@@ -2,6 +2,6 @@ package pt.unl.fct.di.apdc.helpinghand.utility;
 
 public class ClientConfigs {
 
-    public static final String REST_API_URL = "https://helping-hand-312416.ey.r.appspot.com/";
+    public static final String REST_API_URL = "https://thehelpinghand.ew.r.appspot.com/";
             
 }
