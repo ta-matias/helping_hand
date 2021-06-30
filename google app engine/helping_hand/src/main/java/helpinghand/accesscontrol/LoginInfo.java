e9d2758192827e2e9b24e398d14900ea85bb0b9f
unpotpot <afas2809@gmail.com>
2021-06-30 22:02:04 +0100

    Reverted change in LoginInfo
    -Reverted change of varible name from token -> tokenId, now it is token again, it was causing problems on te frontend

--- google app engine/helping_hand/src/main/java/helpinghand/accesscontrol/LoginInfo.java
+++ google app engine/helping_hand/src/main/java/helpinghand/accesscontrol/LoginInfo.java
@@ -4,13 +4,13 @@ public class LoginInfo {
 	
 	public String name;
 	public String role;
-	public String tokenId;
+	public String token;
 	public String expires;
 	
-	public LoginInfo(String name, String role, String tokenId,String expires) {
+	public LoginInfo(String name, String role, String token,String expires) {
 		this.name = name;
 		this.role = role;
-		this.tokenId = tokenId;
+		this.token = token;
 		this.expires = expires;
 	}
 	
